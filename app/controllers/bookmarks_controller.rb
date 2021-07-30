fe378ba89c7b3d2aa64c525fd59a6c2a44f3e070
tahia campos <tahia.campos@gmail.com>
2021-07-30 12:58:34 -0400

    avance

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,16 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new(bookmark_params)
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bookmark_params
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,26 @@
 class ListsController < ApplicationController
   #Ex:- add_index("admin_users", "username")
   def index
+    @list = List.all
   end
+  
+  def show
+    @bookmark = Bookmark.new
+    # @review = Review.new(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
